e8aebde17ba8985596e56c9efadc479e96afc761
miyazatoakira <augusto.akira08@hotmail.com>
2025-02-17 12:14:52 -0300

    feat: added switch color mode

---
+++ usuario/view/assets/js/color-modes.js
@@ -0,0 +1,106 @@
+(() => {
+    "use strict";
+  
+    const getStoredTheme = () => localStorage.getItem("theme");
+    const setStoredTheme = (theme) => localStorage.setItem("theme", theme);
+  
+    const getPreferredTheme = () => {
+      const storedTheme = getStoredTheme();
+      if (storedTheme) {
+        return storedTheme;
+      }
+  
+      return window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
+    };
+  
+    const updateLogo = (theme) => {
+      const logo = document.getElementById("logo-brand");
+      const carousel = document.getElementById("carouselExample");
+  
+      if (!logo) return;
+  
+      if (
+        theme === "dark" ||
+        (theme === "auto" &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+      ) {
+        logo.src = "./assets/images/logo brazil_times white.svg";
+        carousel.className = "carousel slide";
+      } else {
+        logo.src = "./assets/images/logo brazil_times.svg";
+        carousel.className = "carousel carousel-dark slide";
+      }
+    };
+  
+    const setTheme = (theme) => {
+      if (theme === "auto") {
+        document.documentElement.setAttribute(
+          "data-bs-theme",
+          window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light",
+        );
+      } else {
+        document.documentElement.setAttribute("data-bs-theme", theme);
+      }
+      updateLogo(theme);
+    };
+  
+    const showActiveTheme = (theme, focus = false) => {
+      const themeSwitcher = document.querySelector("#bd-theme");
+  
+      if (!themeSwitcher) {
+        return;
+      }
+  
+      const themeSwitcherText = document.querySelector("#bd-theme-text");
+      const activeThemeIcon = document.querySelector(".theme-icon-active use");
+      const btnToActive = document.querySelector(
+        `[data-bs-theme-value="${theme}"]`,
+      );
+      const svgOfActiveBtn = btnToActive
+        .querySelector("svg use")
+        .getAttribute("href");
+  
+      document.querySelectorAll("[data-bs-theme-value]").forEach((element) => {
+        element.classList.remove("active");
+        element.setAttribute("aria-pressed", "false");
+      });
+  
+      btnToActive.classList.add("active");
+      btnToActive.setAttribute("aria-pressed", "true");
+      activeThemeIcon.setAttribute("href", svgOfActiveBtn);
+      const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+      themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
+  
+      if (focus) {
+        themeSwitcher.focus();
+      }
+    };
+  
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", () => {
+        const storedTheme = getStoredTheme();
+        if (storedTheme !== "light" && storedTheme !== "dark") {
+          setTheme(getPreferredTheme());
+        }
+      });
+  
+    window.addEventListener("DOMContentLoaded", () => {
+      const preferredTheme = getPreferredTheme();
+      setTheme(preferredTheme);
+      showActiveTheme(preferredTheme);
+  
+      document.querySelectorAll("[data-bs-theme-value]").forEach((toggle) => {
+        toggle.addEventListener("click", () => {
+          const theme = toggle.getAttribute("data-bs-theme-value");
+          setStoredTheme(theme);
+          setTheme(theme);
+          showActiveTheme(theme, true);
+        });
+      });
+    });
+  })();
